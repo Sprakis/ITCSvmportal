54607878b7a26ea792f4ebd8e89c666fbd04f9d5
Sprakis <yamaletdinov.r2000@gmail.com>
2025-04-19 16:19:16 +0000

    Web_app sync

--- web_app/login_form.js
+++ web_app/login_form.js
@@ -1,14 +1,31 @@
+function togglePassword() {
+	const passwordInput = document.getElementById('password');
+	const eyeIcon = document.getElementById('eye-icon');
+	if (passwordInput.type === 'password') {
+		passwordInput.type = 'text';
+		eyeIcon.classList.remove('fa-eye');
+		eyeIcon.classList.add('fa-eye-slash');
+	} else {
+		passwordInput.type = 'password';
+		eyeIcon.classList.remove('fa-eye-slash');
+		eyeIcon.classList.add('fa-eye');
+	}
+};
+
 function start_func() {
 	let telegramApp = window.Telegram.WebApp;
 	document.querySelector('body').style.backgroundColor = telegramApp.themeParams.secondary_bg_color;
-	telegramApp.expand();
-	telegramApp.MainButton.text = "Sign in";
-	telegramApp.MainButton.color = "#ff0000";
-	telegramApp.MainButton.show();
-	telegramApp.MainButton.onClick(function send() {
-		let login = document.getElementById('username').value;
-		let pass = document.getElementById('password').value;
-		let creds = '{\n\t"login": "' + login + '",\n\t"pass": "' + pass + '"\n}';
-		telegramApp.sendData(creds);
+	telegramApp.expand();	
+
+	document.getElementById('login-form').addEventListener('submit', function(event) {
+		event.preventDefault();
+		let username = document.getElementById('username').value;
+		let password = document.getElementById('password').value;
+	
+		const creds = {
+			login: username,
+			pass: password
+		};
+		telegramApp.sendData(JSON.stringify(creds));
 	});
-};
+}
