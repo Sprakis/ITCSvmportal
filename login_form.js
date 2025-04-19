605f060fd5cba67fafb7d8bd31631ef6218ead26
Sprakis <71209128+Sprakis@users.noreply.github.com>
2025-04-19 19:12:24 +0300

    Update login_form.js

--- login_form.js
+++ login_form.js
@@ -1,20 +1,21 @@
 function togglePassword() {
 	const passwordInput = document.getElementById('password');
-	const showPasswordCheckbox = document.getElementById('show-password');
-	if (showPasswordCheckbox.checked) {
+	const eyeIcon = document.getElementById('eye-icon');
+	if (passwordInput.type === 'password') {
 		passwordInput.type = 'text';
+		eyeIcon.classList.remove('fa-eye');
+		eyeIcon.classList.add('fa-eye-slash');
 	} else {
 		passwordInput.type = 'password';
+		eyeIcon.classList.remove('fa-eye-slash');
+		eyeIcon.classList.add('fa-eye');
 	}
 };
 
 function start_func() {
 	let telegramApp = window.Telegram.WebApp;
-	// document.querySelector('body').style.backgroundColor = telegramApp.themeParams.secondary_bg_color;
-	telegramApp.expand();
-	// telegramApp.MainButton.text = "Sign in";
-	// telegramApp.MainButton.color = "#ff0000";
-	// telegramApp.MainButton.show();	
+	document.querySelector('body').style.backgroundColor = telegramApp.themeParams.secondary_bg_color;
+	telegramApp.expand();	
 
 	document.getElementById('login-form').addEventListener('submit', function(event) {
 		event.preventDefault();
