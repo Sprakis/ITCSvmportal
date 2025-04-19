3ace2fad96a4ce9049e70441b3f1ce6629411b30
Sprakis <yamaletdinov.r2000@gmail.com>
2025-04-19 07:38:15 +0000

    add web_app page

---
+++ web_app/login_form.js
@@ -0,0 +1,18 @@
+function start_func() {
+	let telegramApp = window.Telegram.WebApp;
+	document.querySelector('body').style.backgroundColor = telegramApp.themeParams.secondary_bg_color;
+	telegramApp.expand();
+	telegramApp.MainButton.text = "Sign in";
+	telegramApp.MainButton.color = "#ff0000";
+	telegramApp.MainButton.show();
+
+	function send() {
+		let login = document.getElementById('username').value;
+		let pass = document.getElementById('password').value;
+		let creds = '{\n\t"login": "' + login + '",\n\t"pass": "' + pass + '"\n}';
+		telegramApp.sendData(creds);
+		console.log("send_by_click")
+	}
+
+	telegramApp.MainButton.onClick(send())
+};
