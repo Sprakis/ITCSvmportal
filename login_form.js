316bb58870dd16318b7e2d959998ad65ab617612
Sprakis <yamaletdinov.r2000@gmail.com>
2025-04-19 11:44:01 +0000

    form issue away

--- login_form.js
+++ login_form.js
@@ -15,7 +15,7 @@ function togglePassword() {
 	}
 }
 
-document.getElementById('registration-form').addEventListener('submit', function(event) {
+document.getElementById('login-form').addEventListener('submit', function(event) {
 	event.preventDefault();
 	let username = document.getElementById('username').value;
 	let password = document.getElementById('password').value;
