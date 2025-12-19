1ee6c10bff18abd31e8f11c9bd5c2a872a818e23
Sprakis <yamaletdinov.r2000@gmail.com>
2025-12-19 09:44:38 +0000

    Bot_updater fix

--- Bot_updater.py
+++ Bot_updater.py
@@ -8,6 +8,7 @@ def main() -> None:
 	reset_conf_flag = False
 	reset_env_flag = False
 	branch = "main"
+	msg = "Hello"
 	
 	while True:
 		
@@ -17,11 +18,13 @@ def main() -> None:
 		else:
 			os.system('clear')
 
+		print(f"Status: {msg}")
+
 		key = input(f"""ITCS VM Portal Bot updater\n
 Optinons:\nStream: \033[1m{'\033[92m' if branch == "main" else '\033[93m'}{branch}\033[0m
 Reset config.json: \033[1m{'\033[91m' if reset_conf_flag else '\033[92m'}{reset_conf_flag}\033[0m
 Reset .env: \033[1m{'\033[91m' if reset_env_flag else '\033[92m'}{reset_env_flag}\033[0m
-Choose option:\n1) First phase - Download and unpacked Bot from reposetory\n2) Second phase - Build docker container and upgrade or install bot\n3) Switch reset config flag\n4) Switch reset env flag\n5) Branch select\n6) Delete backups containers\n7) Exit\n\n\n
+Choose option:\n1) First phase - Download and unpacked Bot from repository\n2) Second phase - Build docker container and upgrade or install bot\n3) Switch reset config flag\n4) Switch reset env flag\n5) Branch select\n6) Delete backups containers\n7) Exit\n\n\n
 Optinon: """)
 
 		match int(key):
@@ -56,6 +59,8 @@ Optinon: """)
 				if reset_env_flag:
 					os.system("mv ./Bot/.env_sample ./Bot/.env")
 
+				msg = "Bot downloaded and unpacked"
+
 			case 2:
 				
 				os.chdir("./Bot")
@@ -75,21 +80,23 @@ Optinon: """)
 						if container.name == "itcs_vm_portal_bot":
 							container.stop()
 							container.rename(name = "itcs_vm_portal_bot_backup")
-						if container.name == "itcs_vmp_timerman":
+						if container.name == "itcs_vmpb_timerman":
 							container.stop()
-							container.rename(name = "itcs_vmp_timerman_backup")
+							container.rename(name = "itcs_vmpb_timerman_backup")
 
 				print("Start timerman module")
 				os.system("docker run -it -d --env-file .env -v ./:/bot/ -v /var/log/ITCS_vmpb/:/var/log/ITCS_vmpb/ --restart=unless-stopped --name itcs_vmpb_timerman itcs_vmp_timerman")
 					
 				os.system("docker run -it -d --env-file .env -v ./:/bot/ -v /var/log/ITCS_vmpb/:/var/log/ITCS_vmpb/ --restart=unless-stopped --name itcs_vm_portal_bot itcs_vm_portal")
-
+				
+				msg = "The bot has been successfully installed and launched"
 
 			case 3:
 				if reset_conf_flag:
 					reset_conf_flag = False
 				else:
 					reset_conf_flag = True
+
 			case 4:
 				if reset_env_flag:
 					reset_env_flag = False
@@ -104,10 +111,12 @@ Optinon: """)
 				if 'docker' in sys.modules:
 					docker_client = docker.DockerClient(base_url = 'unix:///var/run/docker.sock')
 					for container in docker_client.containers.list(all = True):
-						if container.name == "itcs_vmp_timerman_backup":
+						if container.name == "itcs_vmpb_timerman_backup":
 							container.remove(v = True)
 						if container.name == "itcs_vm_portal_bot_backup":
 							container.remove(v = True) 
+
+					msg = "Backup containers successfully deleted"
 
 			case 7:
 				return 0
